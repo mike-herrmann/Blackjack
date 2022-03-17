5bcac8f3bbe1831d3ca57104c8f65c70406cf823
mike-herrmann <90936873+mike-herrmann@users.noreply.github.com>
2022-03-17 16:56:42 -0400

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -6,7 +6,7 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eig
           'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 playing = True
-
+# Trying something
 
 class Card:
 
